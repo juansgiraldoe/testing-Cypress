ac71ba62e911905c74075749e4ba546c5b3ea782
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-16 11:08:07 -0500

    [Learn] - Best practices in cypress. {attributes.}

--- cypress/e2e/primer_test.cy.js
+++ cypress/e2e/primer_test.cy.js
@@ -5,8 +5,26 @@ describe('Carga la pagina principal.', () => {
   it('Carga el homepage.', () => {
     cy.visit('http://127.0.0.1:5500/index.html');
 
-    cy.contains('h1', 'Administrador de Pacientes de Veterinaria');
-    cy.get('h1').should('exist');
-  
+    //Verificar el elemento y su texto.
+    cy.contains('[data-cy="titulo-proyecto"]', 'Administrador de Pacientes de Veterinaria');
+
+    //Verificar si existe el elemento.
+    cy.get('[data-cy="titulo-proyecto"]').should('exist');
+    //Buenas practicas con uso de selectores.
+
+    //Verificar que exista un elemento y contenga un texto en especifico.
+    cy.get('[data-cy="titulo-proyecto"]')
+      .invoke('text')
+      .should('equal', 'Administrador de Pacientes de Veterinaria');
+
+    //Verificar el texto de las citas.
+    cy.get('[data-cy=appointments-title]')
+      .invoke('text')
+      .should('equal', 'No hay Citas, comienza creando una');
+    
+      cy.get('[data-cy=appointments-title]')
+      .invoke('text')
+      .should('not.equal', 'Citas');
+
   });
 });
