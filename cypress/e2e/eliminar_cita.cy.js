3b151d573bdf384f3e731c5737aeac446f2c1230
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-16 16:10:10 -0500

    [Learn] - Test delete feature.

---
+++ cypress/e2e/eliminar_cita.cy.js
@@ -0,0 +1,45 @@
+///<reference types = "cypress"/>
+
+describe('LLenar los campos para crear y despues eliminar la cita.', () => {
+  it('Campos nueva cita.', ()=>{
+    cy.visit('/index.html');
+  
+    cy.get('[data-cy=mascota-input]')
+      .type('Hook');
+      
+      cy.get('[data-cy=propietario-input]')
+      .type('Juan');
+      
+      cy.get('[data-cy=telefono-input]')
+      .type('852741');
+      
+      cy.get('[data-cy=fecha-input]')
+      .type('2024-05-10');
+      
+      cy.get('[data-cy=hora-input]')
+      .type('10:45');
+      
+      cy.get('[data-cy=sintomas-input]')
+      .type('Come y duerme mucho.');
+      
+      cy.get('[data-cy=submit-cita]')
+      .click();
+      
+      cy.get('[data-cy=appointments-title]')
+      .invoke('text')
+      .should('equal', 'Administra tus Citas');
+      
+      cy.get('[data-cy="alerta"]')
+      .should('have.class', 'alert-success')
+      .invoke('text')
+      .should('equal', 'Se agregó correctamente')
+    });
+    
+    it('Uliminar una cita', () => {
+      cy.get('[data-cy=btn-eliminar]').click();
+      
+      cy.get('[data-cy=appointments-title]').invoke('text').should('equal', 'No hay Citas, comienza creando una');
+
+    });
+  
+});
